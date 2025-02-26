3f53f87e0792ea8909b3ad16fd976c6ae887cc95
ParkerTraxler <parker_traxler@hotmail.com>
2025-02-26 15:16:54 -0600

    Modified index.html, added edit profile and account deletion

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os, secrets
 from flask import Flask, request, render_template, redirect, url_for, session
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from authlib.integrations.flask_client import OAuth
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
 
@@ -21,6 +22,9 @@ oauth = OAuth(app)
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_USE_SIGNER'] = True
+app.config['SESSION_KEY_PREFIX'] = 'flask_'
+app.config['SESSION_FILE_DIR'] = './.flask_session'
+Session(app)
 
 microsoft = oauth.register(
     name = 'microsoft',
@@ -39,6 +43,12 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False) # should be hashed later
+    firstName = db.Column(db.String(100), nullable=True)
+    lastName = db.Column(db.String(100), nullable=True)
+
+    @property
+    def is_authenticated(self):
+        return True
 
 # creates the database tables
 with app.app_context():
@@ -49,6 +59,12 @@ with app.app_context():
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+
+# update profile form class for manage-account
+class UpdateProfileForm(FlaskForm):
+    first_name = StringField("First Name", validators=[DataRequired(), Length(max=50)])
+    last_name = StringField("Last Name", validators=[DataRequired(), Length(max=50)])
+    submit = SubmitField("Update Profile")
 
 
 # loads the user
@@ -90,7 +106,8 @@ def getAToken():
     token = microsoft.authorize_access_token()
 
     # store the token in the session for future use
-    session['access_token'] = token
+    session['access_token'] = token.get('access_token')
+    session['refresh_token'] = token.get('refresh_token')
 
     # get user information from the Microsoft Graph API, currently we're doing nothing with it
     user_info = microsoft.get('https://graph.microsoft.com/v1.0/me').json()
@@ -99,16 +116,53 @@ def getAToken():
 
     # if the user does not exist, create a new one
     if user is None:
-        user = User(username=user_info['userPrincipalName'], password='')
+        user = User(username=user_info['userPrincipalName'], firstName=user_info['givenName'], lastName=user_info['surname'], password='')
         db.session.add(user)
         db.session.commit()
     
     # Log the user in
     login_user(user)
 
+    # Let Flask-Login know who is logged in
+    session['user_id'] = user.id
+
     #Redirect to the home page
     return(redirect(url_for('home')))
 
+@app.route("/edit-profile", methods=["GET", "POST"])
+@login_required
+def edit_profile():
+    form = UpdateProfileForm()
+
+    if form.validate_on_submit():
+        current_user.firstName = form.first_name.data.strip()
+        current_user.lastName = form.last_name.data.strip()
+        db.session.commit()
+        return(redirect(url_for('home')))
+    
+    # Prefills the fields with existing user data
+    form.first_name.data = current_user.firstName
+    form.last_name.data = current_user.lastName
+
+    return(render_template('edit-profile.html', form=form))
+
+@app.route('/delete-account', methods=['POST'])
+@login_required
+def delete_account():
+    user = User.query.get(current_user.id)
+
+    if user:
+        # delete user from the database
+        db.dession.delete(user)
+        db.session.commit()
+
+        # log out
+        logout_user()
+        session.clear()
+
+        return(redirect(url_for('home')))
+
+    return(redirect(url_for('edit_profile')))
 
 # logout route, logs out the user, obviously
 @app.route('/logout')
